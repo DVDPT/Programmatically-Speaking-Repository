1faaa8ca6890a421f897b77e46216c1ffa619526
DVDPT <amazing.red@gmail.com>
2011-02-23 17:06:30 +0000

    Added IAwaiter Interface Added AwaiterWithCancellation

---
+++ AsyncLike/Extensions/AwaiterWithCancellation.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncLike.Extensions
+{
+    public class AwaiterWithCancellation<T> : IAwaiter<T>
+    {
+  
+
+        private readonly IAwaiter<T> _target;
+        private readonly CancellationToken _token;
+        private bool _wasCancelled;
+
+        public AwaiterWithCancellation(IAwaiter<T> target, CancellationToken token )
+        {
+            _target = target;
+            _token = token;
+        }
+
+        public bool BeginAwait(Action continuation)
+        {
+            if (!_target.BeginAwait(continuation))
+                return false;
+
+            _token.Register(() =>
+                                {
+                                    Action action;
+                                    if (_target.TryCancelAwait(out action))
+                                    {
+                                        _wasCancelled = true;
+                                        action();
+                                    }
+                                });
+            
+            return true;
+        }
+
+        public T EndAwait()
+        {
+            if(_wasCancelled)
+                throw new TaskCanceledException();
+          
+                
+            return _target.EndAwait();
+        }
+
+        public bool TryCancelAwait(out Action continuation)
+        {
+            return _target.TryCancelAwait(out continuation);
+        }
+
+        public IAwaiter<T> GetAwaiter()
+        {
+            return this;
+        }
+    }
+}
---
+++ AsyncLike/Extensions/IAwaiter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AsyncLike.Extensions
+{
+    public interface IAwaiter<out T>
+    {
+        bool BeginAwait(Action continuation);
+        T EndAwait();
+        bool TryCancelAwait(out Action continuation);
+        IAwaiter<T> GetAwaiter();
+    }
+}
